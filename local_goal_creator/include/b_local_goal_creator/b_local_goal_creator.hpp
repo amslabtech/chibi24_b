543b26e4defeb24ae85f39b7d43631c1e11a8786
Takumi-Igarashi0109 <ee213041@meiji.ac.jp>
2024-04-09 12:23:40 +0900

    change name

---
+++ local_goal_creator/include/b_local_goal_creator/b_local_goal_creator.hpp
@@ -0,0 +1,45 @@
+#ifndef B_LOCAL_GOAL_CREATOR_H
+#define B_LOCAL_GOAL_CREATOR_H
+
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+
+class LocalGoalCreator : public rclcpp::Node
+{
+public:
+    LocalGoalCreator();
+    void process();
+    int getOdomFreq();
+
+private:
+    void pathCallback(const nav_msgs::msg::Path::SharedPtr msg);
+    void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+    void publishGoal();
+    double getDistance();
+
+    int hz_;
+    int index_step_;
+    int goal_index_;
+    double taeget_distance_;
+    bool is_path_ = false;
+
+    //　
+    rclcpp::Node::SharedPtr nh_;
+    rclcpp::Node::SharedPtr private_nh_;
+
+    //　Subscriber
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
+
+    // Publisher
+    rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr local_goal_pub_;
+
+
+    nav_msgs::msg::Path path_;
+    geometry_msgs::msg::PointStamped goal_;
+    geometry_msgs::msg::PoseStamped pose_;
+};
+
+#endif // LOCAL_GOAL_CREATOR_H
